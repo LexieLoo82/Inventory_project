b63d854ef75bab00a9ecb81a4d403f88dd56efd2
Alex McIntosh <nz.a.mcintosh@gmail.com>
2018-10-26 20:23:07 +0100

    adds partial new form - products

--- EcoShop_Inventory/controllers/products_controller.rb
+++ EcoShop_Inventory/controllers/products_controller.rb
@@ -9,3 +9,14 @@ get '/products' do
   @products = Product.all()
   erb(:"products/index")
 end
+
+get '/products/new' do
+  @supplies = Supplier.all()
+erb (:"products/new")
+end
+
+post '/products' do
+  @product = Product.new(params)
+  @product.save()
+  redirect to '/products'
+end
--- EcoShop_Inventory/controllers/suppliers_controller.rb
+++ EcoShop_Inventory/controllers/suppliers_controller.rb
@@ -4,3 +4,8 @@ require( 'pry-byebug' )
 require_relative('../models/supplier')
 require_relative('../models/product')
 also_reload( '../models/*' )
+
+get '/suppliers' do
+  @supplierss = Supplier.all
+  erb(:"suppliers/index")
+end
